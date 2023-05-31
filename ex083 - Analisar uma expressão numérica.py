5d2f9fe0598b14b7488d1ac1a18a5498a2cd4ebf
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-05-31 07:32:38 -0300

    Create ex083 - Analisar uma expressão numérica.py

---
+++ ex083 - Analisar uma expressão numérica.py
@@ -0,0 +1,22 @@
+'''Solicitar que o usuário insira uma
+expressão numérica qualquer e avaliar
+se a expressão é válida em relação aos parenteses
+abertos e fechados'''
+
+expr = str(input('Digite uma expressão: '))
+
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão é inválida!')
