191805e400740615d52cc66fa0c40b3312011bae
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-06-01 07:21:54 -0300

    Create ex109.py

---
+++ ex109.py
@@ -0,0 +1,6 @@
+'''
+Modifique as funções que foram criadas no desafio ex107
+para que aceitem um parâmetro a mais, informando
+se o valor retornado por eles vai ser ou não formatado
+pela função moeda(), desenvolvida no desafio 108.
+'''
