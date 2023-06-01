e16eaf960f35bbefe269b3b93bc3ff9d9bb275fb
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-06-01 07:21:12 -0300

    Create ex107.py

---
+++ ex107/ex107.py
@@ -0,0 +1,6 @@
+'''
+Crie um módulo chamado moeda.py que tenha as funções
+incorporadas 'aumentar()', 'diminuir()', 'dobro()' e metade().
+Faça um programa que importe esse módulo e use algumas dessas funções.
+Ler um preço e mostre as funcionalidades...
+'''
