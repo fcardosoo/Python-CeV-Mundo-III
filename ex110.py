57a66a1deacd90bba4062339a253310b2c3ac196
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-06-02 07:13:35 -0300

    Create ex110.py

---
+++ ex110.py
@@ -0,0 +1,6 @@
+'''
+Adicione ao módulo moeda.py criado nos desafios anteriores,
+uma função chamada resumo(), que mostra na tela
+algumas informações geradas pelas funções que já temos
+no módulo criado até aqui.
+'''
