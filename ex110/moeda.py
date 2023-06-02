cc18f3a3955a954defd303a2265abbcadcee1668
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-06-02 07:14:07 -0300

    Create moeda.py

---
+++ ex110/moeda.py
@@ -0,0 +1,34 @@
+def aumentar(preco=0, taxa=0, formato=False):
+    res = preco + (preco * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def diminuir(preco=0, taxar=0, formato=False):
+    res = preco - (preco * taxar / 100)
+    return res if formato is False else moeda(res)
+
+
+def dobro(preco=0, formato=False):
+    res = preco * 2
+    return res if not formato else moeda(res)
+
+
+def metade(preco=0, formato=False):
+    res = preco / 2
+    return res if not formato else moeda(res)
+
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda}{preco:>8.2f}'.replace('.', ',')
+
+
+def resumo (preco=0, taxaa=0, taxar=0):
+    print('-'*35)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 35)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preco, taxaa, True)}')
+    print(f'{taxar}% de redução: \t{diminuir(preco, taxar, True)}')
+    print('-' * 35)
