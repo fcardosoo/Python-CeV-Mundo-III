5406a6844196b0450d21f4228bd56afbdb35a2e3
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-06-02 07:14:59 -0300

    Create ex112.py

---
+++ ex112.py
@@ -0,0 +1,11 @@
+'''
+Dentro do pacote CeV que criamos no desafio 111,
+temos o módulo chamado dado. Crie uma função chamada
+leiaDinheiro() que seja capaz de funcionar como a
+função input(), mas com uma validação de dados para
+aceitar apenas os valores que sejam monetários.
+'''
+
+
+def utilidades():
+    return None
